f9e323d06102302a28e4445ea598dcd450ab163e
Júlio Ramos <ps2.ramos@gmail.com>
2020-07-26 17:21:11 -0300

    Completado o layout base do projeto e adicionada navegação
    Motivo: I/M (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Adicionados componentes base para construção do layout base do
      projeto.
      - Adicionadas rotas para as páginas Dashboard e BillingCycle.
    
    Observações:
    
    Referência:

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default (props) => {
+    return (
+        <footer className='main-footer'>
+            <strong>
+                Copyright &copy; 2017
+                <a href="#" target='_blank'> Learning</a>.
+            </strong>
+        </footer>
+    )
+}
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,11 +1,19 @@
 import '../common/template/dependencies'
 import React from 'react'
 import Header from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
 
 export default (props) => {
     return (
         <div className="wrapper">
             <Header />
+            <Sidebar />
+            <div className='content-wrapper'>
+                <Routes />
+            </div>
+            <Footer />
         </div>
     )
 }
